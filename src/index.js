28e26bcafdf9cbe67c41e5b8731b0eb49fd411dd
jstramigioli <julianstramigioli@gmail.com>
2024-05-04 22:07:36 -0300

    Created getTomorrowWeatherInfo()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './style.css'
+
+async function getTomorrowWeatherInfo(city) {
+    const url = 'https://api.weatherapi.com/v1/forecast.json?'
+    const apiKey = '0ab19ad4300c4d0895923218240305'
+    const fetchData = await fetch(url+'key='+apiKey+'&q='+city+'&days=2')
+    const jsonFetchData = await fetchData.json()
+    const weatherInfo = await function() {
+        const location = jsonFetchData.location
+        const weatherTomorrow = jsonFetchData.forecast.forecastday[1]
+        return {
+            city: location.name,
+            region: location.region,
+            country: location.country,
+            conditionText: weatherTomorrow.day.condition.text,
+            conditionIconUrl: weatherTomorrow.day.condition.text,
+            minTempC: weatherTomorrow.day.mintemp_c,
+            minTempF: weatherTomorrow.day.mintemp_f,
+            maxTempC: weatherTomorrow.day.maxtemp_c,
+            maxTempF: weatherTomorrow.day.maxtemp_f,
+            chanceOfRain: weatherTomorrow.day.daily_chance_of_rain,
+            chanceOfSnow: weatherTomorrow.day.daily_chance_of_snow,
+        }
+    }
+    return weatherInfo()  
+}
+
+getTomorrowWeatherInfo('villa gesell').then(info => console.log(info))
